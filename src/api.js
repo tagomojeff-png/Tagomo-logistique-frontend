889fd5716635258d0cb32a5a0dc19a4578f41132
Jeff Tyson <ton-email-github@gmail.com>
2026-08-12 22:14:04 +0800

    Fix production API connection

--- src/api.js
+++ src/api.js
@@ -3,33 +3,13 @@ import axios from "axios";
 
 const API = axios.create({
 
-    // Backend local pendant les tests
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "https://tagomo-logistique-backend-production.up.railway.app",
 
-    headers: {
-        "Content-Type": "application/json"
+    headers:{
+        "Content-Type":"application/json"
     }
 
 });
-
-
-// Gestion des erreurs API (optionnel mais utile)
-API.interceptors.response.use(
-
-    response => response,
-
-    error => {
-
-        console.log(
-            "Erreur API :",
-            error.response?.data || error.message
-        );
-
-        return Promise.reject(error);
-
-    }
-
-);
 
 
 export default API;
